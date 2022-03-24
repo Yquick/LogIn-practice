be01f99d71a1decab5ccc859e7bb11d363a3d3ed
Yquick <yoonquick@gmail.com>
2022-03-25 03:43:32 +0900

    login2

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
+import React, { useState } from 'react';
 import Login from "./components/Login";
-
+import SignUp from './components/SignUp';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 function App() {
+
+  const [ isLogin, setLogin ] = useState(false)
+
+  const loginHandler = () => {
+    setLogin(true);
+  }
+
   return (
     <div className="App">
-      <Login />
+      <BrowserRouter>
+      { isLogin ? null : <Login loginHandler={loginHandler} /> }
+      </BrowserRouter>
     </div>
   );
 }
