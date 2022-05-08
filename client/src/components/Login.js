f25a31e781f5900c2e7a3027e9ec22b06258eee8
Yquick <yoonquick@gmail.com>
2022-05-08 23:56:29 +0900

    aa

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -14,7 +14,6 @@ import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 const theme = createTheme();
-("a");
 
 export default function SignIn() {
   const handleSubmit = (event) => {
